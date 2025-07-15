226d7804b8b145295c68b7bdf5468fe027e0124f
Gugan <131288000+Gugan-23@users.noreply.github.com>
2025-07-15 11:04:30 +0530

    Update Footer.js

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -26,7 +26,6 @@ function Footer() {
           <h3>Quick Links</h3>
           <ul>
             <li><a href="/">Home</a></li>
-            <li><a href="/product">Products</a></li>
             <li><a href="/about">About</a></li>
           </ul>
         </div>
