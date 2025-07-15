a00420624850a293f3f43c31b985884f605032d4
Gugan <131288000+Gugan-23@users.noreply.github.com>
2025-07-15 15:04:47 +0530

    Update CartPage.js

--- src/pages/Cart/CartPage.js
+++ src/pages/Cart/CartPage.js
@@ -4,7 +4,7 @@ import Price from '../../components/Price/Price';
 import Title from '../../components/Title/Title';
 import { useCart } from '../../hooks/useCart';
 import { FaTrash } from 'react-icons/fa';
-import axios from 'axios';
+import axios from '../axiosConfig'; // ✅ use your config, not plain 'axios'
 import classes from './cartPage.module.css';
 
 export default function CartPage() {
